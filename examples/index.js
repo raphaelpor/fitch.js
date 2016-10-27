696a47631691d4694e9cc85e40849487329bffc1
Raphael Freitas <raphael.freitas@concretesolutions.com.br>
2016-10-27 17:09:23 -0200

    Move examples to examples/

--- examples/index.js
+++ examples/index.js
@@ -1,4 +1,4 @@
-const fetch = require('./fetchService')
+const fetch = require('../src')
 
 const reqUpdate = {
   body: { name: 'Updated cat' },
@@ -10,25 +10,25 @@ const reqPost = {
 
 // GET
 fetch.get('cats').then(resp =>
-  console.log('GET >>>', resp)
+  console.log('GET\t>>>', resp)
 ).catch(fetch.error)
 
 // POST
 fetch.post('cats/new/', reqPost).then(resp =>
-  console.log('POST >>>', resp)
+  console.log('POST\t>>>', resp)
 ).catch(fetch.error)
 
 // PUT
 fetch.put('cats/1', reqUpdate).then(resp =>
-  console.log('PUT >>>', resp)
+  console.log('PUT\t>>>', resp)
 ).catch(fetch.error)
 
 // PATCH
 fetch.patch('cats/2', reqUpdate).then(resp =>
-  console.log('PATCH >>>', resp)
+  console.log('PATCH\t>>>', resp)
 ).catch(fetch.error)
 
 // DELETE
 fetch.delete('cats/2').then(resp =>
-  console.log('DELETE >>>', resp)
+  console.log('DELETE\t>>>', resp)
 ).catch(fetch.error)
