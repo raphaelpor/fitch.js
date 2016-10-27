d046c73c2cf4ffd1c4e4f346d57e94e745e37a2c
Raphael Freitas <raphael.freitas@concretesolutions.com.br>
2016-10-27 12:58:39 -0200

    Add delete request

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const fetch = require('./fetchService')
 
+// GET
 fetch.get('cats').then(resp =>
   console.log('GET >>>', resp)
 ).catch(fetch.error)
 
+// POST
 fetch.post('cats/new/', { name: 'New cat' }).then(resp =>
   console.log('POST >>>', resp)
 ).catch(fetch.error)
 
+// PUT
 fetch.put('cats/1', { name: 'Updated cat' }).then(resp =>
   console.log('PUT >>>', resp)
+).catch(fetch.error)
+
+// DELETE
+fetch.delete('cats/2').then(resp =>
+  console.log('DELETE >>>', resp)
 ).catch(fetch.error)
