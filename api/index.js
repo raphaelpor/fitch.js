eaf1a839071da9b1a02301050f894dcce7c94236
Raphael Freitas <raphael.freitas@concretesolutions.com.br>
2016-10-27 12:39:34 -0200

    Update api

--- api/index.js
+++ api/index.js
@@ -1,47 +1,48 @@
 'use strict';
 
-const Hapi = require('hapi');
+const Hapi = require('hapi')
 
-// Create a server with a host and port
-const server = new Hapi.Server();
+const server = new Hapi.Server()
 server.connection({
   host: 'localhost',
   port: 8000
-});
+})
+
+const path = '/cats'
 
 server.route({
   method: 'GET',
-  path:'/cats',
-  handler: function (request, reply) {
+  path: path,
+  handler: function(request, reply) {
     const cats = [
       { name: 'Grumpy cat' },
       { name: 'Happy cat' },
-    ];
+    ]
 
-    return reply(cats);
+    return reply(cats)
   }
-});
+})
 
 server.route({
   method: 'POST',
-  path:'/cats/new/',
-  handler: function (request, reply) {
-    return reply(request.payload);
+  path: `${path}/new/`,
+  handler: function(request, reply) {
+    return reply(request.payload)
   }
-});
+})
 
 server.route({
   method: 'PUT',
-  path:'/cats/{id}',
-  handler: function (request, reply) {
-    return reply(request.payload);
+  path: `${path}/{id}`,
+  handler: function(request, reply) {
+    return reply(request.payload)
   }
-});
+})
 
 // Start the server
 server.start(err => {
   if (err) {
-    throw err;
+    throw err
   }
-  console.log(`Server running at: ${server.info.uri}`);
-});
+  console.log(`Server running at: ${server.info.uri}`)
+})
