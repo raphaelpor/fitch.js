edbbbe66ff9ae7d397058a9f95f0f1b439e1a6bb
Raphael Porto <raphaelpor@gmail.com>
2016-10-26 23:56:43 -0200

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,41 @@
+export default {
+  API_URL: 'https://api.github.com/users/',
+
+  get(url) {
+    return this.request('GET', url)
+  },
+
+  post(url, body) {
+    return this.request('POST', url, body)
+  },
+
+  put(url) {
+    return this.request('PUT', url)
+  },
+
+  delete(url) {
+    return this.request('DELETE', url)
+  },
+
+  request(method, url, body) {
+    const config = this.config(method, body)
+    return fetch(this.API_URL + url, config).then(this.check)
+  },
+
+  config(method, body) {
+    const obj = {
+      method,
+      body,
+      headers: { "Content-Type": "application/json" }
+    }
+    return obj
+  },
+
+  check(resp) {
+    return resp.ok && resp.json()
+  },
+
+  error(err) {
+    console.log(err);
+  },
+}
