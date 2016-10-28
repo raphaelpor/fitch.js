70636a578f058a870a6772ad03768235ef70f0d2
Raphael Porto <raphaelpor@gmail.com>
2016-10-28 21:57:55 -0200

    Add semicolons

--- examples/index.js
+++ examples/index.js
@@ -1,36 +1,36 @@
-const fitch = require('../src')
+const fitch = require('../src');
 
-const baseUrl = 'http://localhost:8000/cats'
+const baseUrl = 'http://localhost:8000/cats';
 
 const reqUpdate = {
   body: { name: 'Updated cat' },
-}
+};
 
 const reqPost = {
   body: { name: 'New cat' },
-}
+};
 
 // GET
 fitch.get(baseUrl).then(resp =>
   console.log('GET\t>>>', resp)
-).catch(fitch.error)
+).catch(fitch.error);
 
 // POST
 fitch.post(`${baseUrl}/new/`, reqPost).then(resp =>
   console.log('POST\t>>>', resp)
-).catch(fitch.error)
+).catch(fitch.error);
 
 // PUT
 fitch.put(`${baseUrl}/1`, reqUpdate).then(resp =>
   console.log('PUT\t>>>', resp)
-).catch(fitch.error)
+).catch(fitch.error);
 
 // PATCH
 fitch.patch(`${baseUrl}/2`, reqUpdate).then(resp =>
   console.log('PATCH\t>>>', resp)
-).catch(fitch.error)
+).catch(fitch.error);
 
 // DELETE
 fitch.delete(`${baseUrl}/2`).then(resp =>
   console.log('DELETE\t>>>', resp)
-).catch(fitch.error)
+).catch(fitch.error);
--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
 // Verify if is running in browser
 const nodeFetch = require('node-fetch');
 
-const f = typeof window === 'undefined' ? nodeFetch : fetch
+const f = typeof window === 'undefined' ? nodeFetch : fetch;
 
 const config = require('./config');
 
 module.exports = {
   get(url, req) {
-    return this.request('GET', url, req)
+    return this.request('GET', url, req);
   },
 
   post(url, req) {
-    return this.request('POST', url, req)
+    return this.request('POST', url, req);
   },
 
   put(url, req) {
-    return this.request('PUT', url, req)
+    return this.request('PUT', url, req);
   },
 
   patch(url, req) {
-    return this.request('PATCH', url, req)
+    return this.request('PATCH', url, req);
   },
 
   delete(url, req) {
-    return this.request('DELETE', url, req)
+    return this.request('DELETE', url, req);
   },
 
   request(method, url, req) {
-    const params = config.create(method, req)
-    return f(url, params).then(this.check)
+    const params = config.create(method, req);
+    return f(url, params).then(this.check);
   },
 
   check(resp) {
     if (resp.ok) {
-      return resp.json()
+      return resp.json();
     }
-    throw new Error(`${resp.status} - ${resp.statusText}.`)
+    throw new Error(`${resp.status} - ${resp.statusText}.`);
   },
 
   error(err) {
     console.log('Error >', err);
   },
-}
+};
