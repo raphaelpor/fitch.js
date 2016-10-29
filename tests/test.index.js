2f0cd3e57a243b9173962cd7fcef4f811d5d5261
Raphael Porto <raphaelpor@gmail.com>
2016-10-29 15:30:56 -0200

    Update unit tests
    Add tests for request(), check() and error().

--- tests/test.index.js
+++ tests/test.index.js
@@ -13,25 +13,50 @@ const reqPost = {
 
 test('method: get', t =>
   fitch.get(baseUrl)
-    .then(data => t.pass())
+    .then(data => t.is(typeof data, 'object', 'data is a object'))
 )
 
 test('method: post', t =>
   fitch.post(`${baseUrl}/new/`, reqPost)
-    .then(data => t.pass())
+    .then(data => t.is(typeof data, 'object', 'data is a object'))
 )
 
 test('method: put', t =>
   fitch.put(`${baseUrl}/1`, reqUpdate)
-    .then(data => t.pass())
+    .then(data => t.is(typeof data, 'object', 'data is a object'))
 )
 
 test('method: patch', t =>
   fitch.patch(`${baseUrl}/1`, reqUpdate)
-    .then(data => t.pass())
+    .then(data => t.is(typeof data, 'object', 'data is a object'))
 )
 
 test('method: delete', t =>
   fitch.delete(`${baseUrl}/1`)
-    .then(data => t.pass())
+    .then(data => t.is(typeof data, 'object', 'data is a object'))
 )
+
+test('method: request', t => {
+  t.is(typeof fitch.request, 'function', 'is defined and is a function')
+})
+
+test('method: check', t => {
+  const resp = {
+    ok: true,
+    json() {
+      return true
+    },
+    status: '404',
+    statusText: 'Not found.',
+  }
+
+  t.is(typeof fitch.check, 'function', 'is defined and is a function')
+  t.is(fitch.check(resp), true, 'validade the response and return "true"')
+})
+
+test('method: error', t => {
+  const err = 'test message'
+
+  t.is(typeof fitch.error, 'function', 'is defined and is a function')
+  t.is(fitch.error(err), undefined, 'log the error on console')
+})
