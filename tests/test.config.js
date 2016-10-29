36c60a8733580eeced900c6650828d1b3bccc065
Raphael Porto <raphaelpor@gmail.com>
2016-10-29 16:06:11 -0200

    Update unit tests
    Add tests for config.create() method.

--- tests/test.config.js
+++ tests/test.config.js
@@ -5,6 +5,26 @@ test('module: config', t =>
   t.is(typeof config, 'object', 'is defined and is a object')
 )
 
-test('method: create', t =>
+test('method: create', t => {
+  const conf = {
+    body: { name: 'test' },
+    cache: 'no-store',
+    headers: { 'Content-Type': 'plain/text' },
+    headersDefault: 'application/json',
+    mode: 'no-cors',
+  }
+
   t.is(typeof create, 'function', 'is defined and is a function')
-)
+
+  t.is(create().body, JSON.stringify(''), 'body = ""')
+  t.is(create('', conf).body, JSON.stringify(conf.body), 'body = "test"')
+
+  t.is(create().cache, 'default', 'cache = "default"')
+  t.is(create('', conf).cache, conf.cache, 'cache = "no-store"')
+
+  t.is(create().headers['Content-Type'], conf.headersDefault, 'headers = "application/json"')
+  t.is(create('', conf).headers, conf.headers, 'headers = "no-cors"')
+
+  t.is(create().mode, 'cors', 'mode = "cors"')
+  t.is(create('', conf).mode, conf.mode, 'mode = "no-cors"')
+})
