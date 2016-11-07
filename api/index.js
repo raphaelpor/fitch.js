1159d13947e86f17414698597b86cc9721081334
Raphael Porto <raphaelpor@gmail.com>
2016-11-07 21:02:22 -0200

    Apply lint rules to the api example

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
-const Hapi = require('hapi') // eslint-disable-line
+const Hapi = require('hapi'); // eslint-disable-line
 
-const server = new Hapi.Server()
+const server = new Hapi.Server();
 server.connection({
   host: 'localhost',
   port: 8000,
-})
+});
 
-const path = '/cats'
+const path = '/cats';
 
 server.route({
   method: 'GET',
@@ -15,48 +15,48 @@ server.route({
     const cats = [
       { name: 'Grumpy cat' },
       { name: 'Happy cat' },
-    ]
+    ];
 
-    return reply(cats)
+    return reply(cats);
   },
-})
+});
 
 server.route({
   method: 'POST',
   path: `${path}/new/`,
   handler(request, reply) {
-    return reply(request.payload)
+    return reply(request.payload);
   },
-})
+});
 
 server.route({
   method: 'PUT',
   path: `${path}/{id}`,
   handler(request, reply) {
-    return reply(request.payload)
+    return reply(request.payload);
   },
-})
+});
 
 server.route({
   method: 'PATCH',
   path: `${path}/{id}`,
   handler(request, reply) {
-    return reply(request.payload)
+    return reply(request.payload);
   },
-})
+});
 
 server.route({
   method: 'DELETE',
   path: `${path}/{id}`,
   handler(request, reply) {
-    return reply({ response: 'ok' })
+    return reply();
   },
-})
+});
 
 // Start the server
 server.start((err) => {
   if (err) {
-    throw err
+    throw err;
   }
-  console.log(`Server running at: ${server.info.uri}`)
-})
+  console.log(`Server running at: ${server.info.uri}`);
+});
