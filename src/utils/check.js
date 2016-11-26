dc2d22d1ba7ba20565e5e8d6e9b30faa6b41aed1
Raphael Porto <raphaelpor@gmail.com>
2016-11-26 17:12:42 -0200

    Update default response to have some headers

--- src/utils/check.js
+++ src/utils/check.js
@@ -1,12 +1,20 @@
 function check(resp, dataType = 'json') {
   const typeList = ['arrayBuffer', 'blob', 'formData', 'json', 'text'];
-  const included = typeList.indexOf(dataType);
+  const included = typeList.indexOf(dataType) !== -1;
 
-  if (resp.ok && included !== -1) {
-    return resp[dataType]();
+  if (resp.ok && included) {
+    return resp[dataType]()
+      .then(data => ({
+        data,
+        status: resp.status,
+        statusText: resp.statusText,
+        headers: resp.headers,
+      }));
   }
 
-  throw new Error(`${resp.status} - ${resp.statusText}.`);
+  const errorMerssage = !included ? 'Invalid data type' : `${resp.status} - ${resp.statusText}.`;
+
+  throw new Error(errorMerssage);
 }
 
 export default check;
