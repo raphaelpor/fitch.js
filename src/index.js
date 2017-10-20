45413a69fb2f5623b6b2c97931c086cb4f6a46eb
arthurfranca <arthur.a.franca@gmail.com>
2017-10-20 17:32:34 -0200

    Add initializer with custom request options

--- src/index.js
+++ src/index.js
@@ -1,45 +1,64 @@
+import merge from 'lodash.merge';
 import request from './utils/request';
 
-function get(url, req) {
-  return request('GET', url, req);
+function intializer(rqst) {
+  function get(url, req) {
+    return rqst('GET', url, req);
+  }
+
+  function post(url, req) {
+    return rqst('POST', url, req);
+  }
+
+  function put(url, req) {
+    return rqst('PUT', url, req);
+  }
+
+  function patch(url, req) {
+    return rqst('PATCH', url, req);
+  }
+
+  function del(url, req) {
+    return rqst('DELETE', url, req);
+  }
+
+  function all(promises) {
+    return Promise.all(promises);
+  }
+
+  function init({
+      config = {},
+      interceptor = promise => promise,
+    } = {}) {
+    const customRqst = (method, url, req) => (
+      request(method, url, merge({}, config, req))
+        .then(interceptor)
+    );
+
+    return intializer(customRqst);
+  }
+
+  return {
+    all,
+    del,
+    get,
+    init,
+    patch,
+    post,
+    put,
+  };
 }
 
-function post(url, req) {
-  return request('POST', url, req);
-}
-
-function put(url, req) {
-  return request('PUT', url, req);
-}
-
-function patch(url, req) {
-  return request('PATCH', url, req);
-}
-
-function del(url, req) {
-  return request('DELETE', url, req);
-}
-
-function all(promises) {
-  return Promise.all(promises);
-}
-
-const fitch = {
-  all,
-  del,
-  get,
-  patch,
-  post,
-  put,
-};
+const fitch = intializer(request);
 
 export default fitch;
 
-export {
+export const {
   all,
   del,
   get,
+  init,
   patch,
   post,
   put,
-};
+} = fitch;
--- tests/test.index.js
+++ tests/test.index.js
@@ -1,5 +1,5 @@
 import test from 'ava'
-import { get, put, post, patch, del, all, error } from '../src'
+import fitch, { get, put, post, patch, del, all, init, error } from '../src'
 
 const baseUrl = 'http://localhost:8000/cats'
 
@@ -9,6 +9,13 @@ const reqUpdate = {
 
 const reqPost = {
   body: { name: 'New cat' },
+}
+
+const initOpts = {
+  config: { body: { friendName: 'Special Cat' } },
+  interceptor: ({ data: { name, friendName } }) => (
+    name === 'New cat' && friendName === 'Special Cat'
+  )
 }
 
 test('method: get', t =>
@@ -52,4 +59,13 @@ test('method: all', t => {
       t.is(result1, 'ok-1', 'result1 is ok-1')
       t.is(result2, 'ok-2', 'result2 is ok-2')
     })
+})
+
+test('method: init', t => {
+  t.not(fitch.init(), fitch)
+
+  return init(initOpts)
+    .post(`${baseUrl}/new/`, reqPost)
+    .then(isExpectedReturn => t.true(isExpectedReturn,'config and interceptor applied to request'))
+    .catch(data => t.pass())
 })
